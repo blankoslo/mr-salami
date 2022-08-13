82ad9db4a9cefec7ed89dd286b2b6d924eeaffc2
Kim Midtlid <kmidtlid3@gmail.com>
2022-08-13 12:35:15 +0200

    Sort upcoming event by ascending order

--- db.py
+++ db.py
@@ -208,7 +208,7 @@ def get_future_pizza_events():
         GROUP BY
             events.id
         ORDER BY
-            time DESC;
+            time ASC;
     """
     with pizza_conn:
         with pizza_conn.cursor() as curs:
