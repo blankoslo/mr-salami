ed496aa405dd910279be79bf4fec5665db627010
Theodor René Carlsen <theodor.carlsen@blank.no>
2022-08-15 13:31:38 +0200

    add easier way to change to prod url

--- frontend/src/queries.ts
+++ frontend/src/queries.ts
@@ -1,17 +1,21 @@
 import { INewPizzaEvent } from "types";
 
+const PROD = "https://oppstart22-gruppe2-pizzabot.herokuapp.com"
+const DEV = "http://localhost:8080"
+const URL = DEV
+
 export const fetchAllPizzaEvents = async () => {
-  const res = await fetch(`http://localhost:8080/api/events`);
+  const res = await fetch(`${URL}/api/events`);
   return res.json();
 }
 
 export const fetchUpcomingPizzaEvents = async () => {
-  const res = await fetch(`http://localhost:8080/api/future_events`);
+  const res = await fetch(`${URL}/api/future_events`);
   return res.json();
 }
 
 export const postNewPizzaEvent = async (data : INewPizzaEvent) => {
-    const res = await fetch('http://localhost:8080/api/events', {
+    const res = await fetch(`${URL}/api/events`, {
         method: 'POST',
         headers: {
         'Accept': 'application/json',
